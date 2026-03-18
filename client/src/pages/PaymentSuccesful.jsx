1beb90e3f903b416fa9e5a4e76d68f55485aba65
JavadAlip <javadalipp911@gmail.com>
2026-03-18 09:33:15 +0530

    Changes

--- client/src/pages/PaymentSuccesful.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PaymentMain from '../components/PaymentSuccessPage/PaymentMain';
-
-const PaymentSuccesful = () => {
-  return <PaymentMain />;
-};
-
-export default PaymentSuccesful;
